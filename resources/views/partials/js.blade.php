e3a85d711fc8ee19ba7cc45768181ac0ba8bd166
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2023-11-12 17:35:28 +0500

    'reformat'

--- resources/views/partials/js.blade.php
+++ resources/views/partials/js.blade.php
@@ -1,5 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js" integrity="sha512-QSkVNOCYLtj73J4hbmVoOV6KVZuMluZlioC+trLpewV8qMjsWqlIQvkn1KGX2StWvPMdWGBqim1xlC8krl1EKQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 <script src="{{asset('calm/js/vendors.js')}}"></script>
--- resources/views/user/meditation/play.blade.php
+++ resources/views/user/meditation/play.blade.php
@@ -64,29 +64,7 @@
                               <div class="testimonials__author">
                                 <h5 class="text-17 lh-15 fw-500">Ali Tufan</h5>
                                 <div class="mt-5">Product Manager, Apple Inc</div>
-                                {{-- <audio controls>
-                                    <source src="{{asset('calm/media/test.mp3')}}" type="audio/mpeg">
-                                  </audio> --}}
-                                  <div class="container">
-                                    <div class="column add-bottom">
-                                        <div id="mainwrap">
-                                            <div id="nowPlay">
-                                                <span id="npAction">Paused...</span><span id="npTitle"></span>
-                                            </div>
-                                            <div id="audiowrap">
-                                                <div id="audio0">
-                                                    <audio id="audio1" preload controls>Your browser does not support HTML5 Audio! 😢</audio>
-                                                </div>
-                                                <div id="tracks">
-                                                    <a id="btnPrev">&larr;</a><a id="btnNext">&rarr;</a>
-                                                </div>
-                                            </div>
-                                            <div id="plwrap">
-                                                <ul id="plList"></ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                
 
                               </div>
                             </div>
