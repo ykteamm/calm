aa81a638e5da7c0fced10c5198880057f4ed74ec
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2024-01-24 15:14:30 +0500

    gfgf

--- resources/views/livewire/gratitude.blade.php
+++ resources/views/livewire/gratitude.blade.php
@@ -1,17 +1,17 @@
 <div class="modal-content" style="background: #1a4d73">
     <div class="modal-header row" style="border-bottom: none">
-        <div class="col-md-11 col-sm-11 col-11">
+        <div class="col-md-10 col-sm-10 col-10">
             <h5 class="modal-title text-color-white-for" id="exampleModalLabel">
                 {{$gratitude->translation->name}}
             </h5>
         </div>
-        <div class="col-md-1 col-sm-1 col-1">
+        <div class="col-md-2 col-sm-2 col-2">
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <br><br>
         <button wire:click="changeQuestion({{$gratitude}})" class="text-white">
             <i class="fas fa-exchange-alt mr-10"></i>
-                Change question?
+                Savolni almashtirish?
         </button>
         {{-- <div class="col-md-12 text-center">
             <h6 class="modal-title text-color-white-for" id="question" style="cursor: pointer;">
@@ -30,21 +30,21 @@
             <div class="row">
                 <div class="col-12 d-flex align-items-center">
                     <label class="text-16 lh-1 fw-500 text-dark-1 mb-10 text-color-white-for">1.</label>
-                    <input type="text" class="placeholder123" required name="titles[]" placeholder="I'm grateful for...">
+                    <input type="text" class="placeholder123" required name="titles[]" placeholder="Men minnatdorman...">
                 </div>
                 <div class="col-12 d-flex align-items-center">
                     <label class="text-16 lh-1 fw-500 text-dark-1 mb-10 text-color-white-for">2.</label>
-                    <input type="text" class="placeholder123" name="titles[]" placeholder="I'm grateful for...">
+                    <input type="text" class="placeholder123" name="titles[]" placeholder="Men minnatdorman...">
                 </div>
                 <div class="col-12 d-flex align-items-center">
                     <label class="text-16 lh-1 fw-500 text-dark-1 mb-10 text-color-white-for">3.</label>
-                    <input type="text" class="placeholder123" name="titles[]" placeholder="I'm grateful for...">
+                    <input type="text" class="placeholder123" name="titles[]" placeholder="Men minnatdorman...">
                 </div>
             </div>
             <div class="modal-footer" style="border: none">
                 <button type="submit" class="btn btn-primary">
-                    <i class="fas fa-plus-circle"></i>
-                    Create
+                    {{-- <i class="fas fa-plus-circle"></i> --}}
+                    Saqlash
                 </button>
             </div>
         </form>
